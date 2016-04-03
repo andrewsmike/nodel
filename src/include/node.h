68c3d0942e4f17cd7c14b6bacd0045e18520ef9e
Michael Andrews <mandrews@wpi.edu>
2016-04-03 18:29:45 -0400

    Improve node header.

--- src/include/node.h
+++ src/include/node.h
@@ -3,36 +3,76 @@
 
 #include <stdint.h>
 
-typedef enum ndl_value_type_e {
 
-    ENONE  = -1,   // Unknown
-    EREF   =  0,   // Reference to other node.
-    ESYM   =  1,   // 8 char long string. Padded with spaces, no NULL terminator.
-    EINT   =  2,   // Integer value.
-    EFLOAT =  3,   // Floating point value.
+/* Values hold either data or references to nodes.
+ * Supported datatypes are ints, floats, and symbols.
+ * References can be NULL. Nodes are capped at 65k.
+ *
+ * You can perform boolean operations on integers.
+ * You can perform basic math operations on integers and floats.
+ *
+ * Symbols are unsigned 64 bit ints. They can be converted to/from integers.
+ * Typically, symbols will represent a string like so: *((uint64_t*)"somesym ").
+ * Strings will be lower case, 8 long, space padded, unterminated.
+ * Endian is left to the platform.
+ *
+ * Functions that return a nodel value may use type=ENONE as an error indicator.
+ */
 
-} ndl_value_type;
+
+enum ndl_value_type_e {
+
+    EVAL_NONE,  /* Invalid value. */
+
+    EVAL_REF,   /* Reference to a node. */
+    EVAL_SYM,   /* Symbol. */
+    EVAL_INT,   /* Integer number. */
+    EVAL_FLOAT, /* Floating point number. */
+
+    EVAL_SIZE
+
+};
+
+/* Get a pretty-print unpadded names for each type. Max strlen() = 5. */
+extern const char *ndl_value_type_to_string[EVAL_SIZE];
 
 
-typedef int32_t ndl_ref;
-#define NDL_NULL ((ndl_ref) -1)
-
+/* C datatypes for each value datatype. */
+typedef  int16_t ndl_ref;
 typedef uint64_t ndl_sym;
-#define NDL_NULL_SYM ((ndl_sym) 0)
-typedef int64_t ndl_int;
-typedef double ndl_float;
+typedef  int64_t ndl_int;
+typedef double   ndl_float;
+
+/* NULL / invalid values for some datatypes and other macros. */
+#define NDL_NULL_REF ((ndl_ref) -1)
+#define NDL_NULL_SYM ((ndl_sym)  0)
+
+#define NDL_SYM(sym) *((uint64_t*) sym)
 
 
-typedef struct ndl_value_s {
+/* Actual value structure. */
+struct ndl_value_s {
 
-    ndl_value_type type;
+    enum ndl_value_type_e type;
     union {
-        ndl_ref ref; // EREF
-        ndl_sym sym; // ESYM
-        ndl_int num; // EINT
-        ndl_float real; // EREAL
+        ndl_ref   ref;  /* EVAL_REF   */
+        ndl_sym   sym;  /* EVAL_SYM   */
+        ndl_int   num;  /* EVAL_INT   */
+        ndl_float real; /* EVAL_FLOAT */
     };
+};
+typedef struct ndl_value_s ndl_value;
 
-} ndl_value;
+/* Pretty prints a value.
+ * NULL terminates, returns strlen(buff).
+ */
+int ndl_value_to_string(ndl_value value, int len, const char *buff);
+
+/* Value construction macro.
+ * Usage: NDL_VALUE(EVAL_SYM, sym=NDL_SYM("hello   "))
+ *        NDL_VALUE(EVAL_INT, num=27)
+ */
+#define NDL_VALUE(type_e, value) ((ndl_value) {.type = type_e, .value})
+
 
 #endif /* NODEL_NODE_H */
