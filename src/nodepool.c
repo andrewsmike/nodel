2f70a85f36be364d50a0eb489ace7778bcc83d1e
Michael Andrews <mandrews@wpi.edu>
2016-04-08 12:34:43 -0400

    Minor documentation change, make nodepool slightly more bearable through iterators.

--- src/nodepool.c
+++ src/nodepool.c
@@ -96,6 +96,41 @@ ndl_sym ndl_kv_node_index(ndl_kv_node *node, int index) {
     return node->key;
 }
 
+void *ndl_kv_node_head(ndl_kv_node *node) {
+
+    return (void *) node;
+}
+
+void *ndl_kv_node_next(void *last) {
+
+    if (last == NULL)
+        return NULL;
+
+    ndl_kv_node *lastkv = last;
+
+    return lastkv->next;
+}
+
+ndl_sym ndl_kv_node_key(void *curr) {
+
+    if (curr == NULL)
+        return NDL_NULL_SYM;
+
+    ndl_kv_node *currkv = curr;
+
+    return currkv->key;
+}
+
+ndl_value ndl_kv_node_val(void *curr) {
+
+    if (curr == NULL)
+        return NDL_VALUE(EVAL_NONE, ref=NDL_NULL_REF);
+
+    ndl_kv_node *currkv = curr;
+
+    return currkv->val;
+}
+
 void nld_kv_node_print(ndl_kv_node *node) {
 
     char keybuff[16], valbuff[16];
@@ -119,6 +154,8 @@ ndl_node_pool *ndl_node_pool_init(void) {
     for (int i = 0; i < NDL_MAX_NODES; i++)
         pool->slots[i] = NULL;
 
+    pool->use = 0;
+
     return pool;
 }
 
@@ -136,6 +173,7 @@ ndl_ref ndl_node_pool_alloc(ndl_node_pool *pool) {
     for (int i = 0; i < NDL_MAX_NODES; i++) {
         if (pool->slots[i] == NULL) {
             pool->slots[i] = ndl_kv_node_push(NULL, *((uint64_t*)"self    "), (ndl_value){.type=EVAL_REF, .ref=i});
+            pool->use++;
             return (ndl_ref) i;
         }
     }
@@ -150,6 +188,7 @@ void ndl_node_pool_free(ndl_node_pool *pool, ndl_ref node) {
 
     ndl_kv_node_free(pool->slots[node]);
 
+    pool->use--;
     pool->slots[node] = NULL;
 
     return;
@@ -208,6 +247,63 @@ ndl_sym ndl_node_pool_get_key(ndl_node_pool *pool, ndl_ref node, int index) {
         return -1;
 
     return ndl_kv_node_index(pool->slots[node], index);
+}
+
+int ndl_node_pool_size(ndl_node_pool *pool) {
+
+    return pool->use;
+}
+
+static inline ndl_ref ndl_node_pool_scan(ndl_node_pool *pool, ndl_ref start) {
+
+    int i;
+    for (i = (int) start; i < NDL_MAX_NODES; i++)
+        if (pool->slots[i] != NULL)
+            return (ndl_ref) i;
+
+    return -1;
+}
+
+ndl_ref ndl_node_pool_head(ndl_node_pool *pool) {
+
+    return ndl_node_pool_scan(pool, (ndl_ref) 0);
+}
+
+ndl_ref ndl_node_pool_next(ndl_node_pool *pool, ndl_ref last) {
+
+    if (last == NDL_NULL_REF)
+        return -1;
+
+    if (last >= (NDL_MAX_NODES - 1))
+        return -1;
+
+    return ndl_node_pool_scan(pool, last + 1);
+}
+
+void *ndl_node_pool_kv_head(ndl_node_pool *pool, ndl_ref node) {
+
+    if (node == NDL_NULL_REF)
+        return NULL;
+
+    if (node > NDL_MAX_NODES)
+        return NULL;
+
+    return ndl_kv_node_head(pool->slots[(int) node]);
+}
+
+void *ndl_node_pool_kv_next(ndl_node_pool *pool, ndl_ref node, void *last) {
+
+    return ndl_kv_node_next(last);
+}
+
+ndl_sym ndl_node_pool_kv_key(ndl_node_pool *pool, ndl_ref node, void *curr) {
+
+    return ndl_kv_node_key(curr);
+}
+
+ndl_value ndl_node_pool_kv_val(ndl_node_pool *pool, ndl_ref node, void *curr) {
+
+    return ndl_kv_node_val(curr);
 }
 
 void ndl_node_pool_print(ndl_node_pool *pool) {
--- src/nodepool.h
+++ src/nodepool.h
@@ -31,8 +31,15 @@ ndl_kv_node *ndl_kv_node_remove(ndl_kv_node *node, ndl_sym key);
 int ndl_kv_node_depth(ndl_kv_node *node);
 ndl_sym ndl_kv_node_index(ndl_kv_node *node, int index);
 
+/* Iterators. */
+void *ndl_kv_node_head(ndl_kv_node *node);
+void *ndl_kv_node_next(void *last);
+ndl_sym  ndl_kv_node_key(void *curr);
+ndl_value ndl_kv_node_val(void *curr);
+
 #define NDL_MAX_NODES 4096
 typedef struct ndl_node_pool_s {
+    int use;
     ndl_kv_node *slots[NDL_MAX_NODES];
 } ndl_node_pool;
 
@@ -48,6 +55,16 @@ ndl_value ndl_node_pool_get(ndl_node_pool *pool, ndl_ref node, ndl_sym key);
 
 int ndl_node_pool_get_size(ndl_node_pool *pool, ndl_ref node);
 ndl_sym ndl_node_pool_get_key(ndl_node_pool *pool, ndl_ref node, int index);
+
+ndl_ref ndl_node_pool_head(ndl_node_pool *pool);
+ndl_ref ndl_node_pool_next(ndl_node_pool *pool, ndl_ref last);
+
+void *ndl_node_pool_kv_head(ndl_node_pool *pool, ndl_ref node);
+void *ndl_node_pool_kv_next(ndl_node_pool *pool, ndl_ref node, void *last);
+ndl_sym   ndl_node_pool_kv_key(ndl_node_pool *pool, ndl_ref node, void *curr);
+ndl_value ndl_node_pool_kv_val(ndl_node_pool *pool, ndl_ref node, void *curr);
+
+int ndl_node_pool_size(ndl_node_pool *pool);
 
 void ndl_node_pool_print(ndl_node_pool *pool);
 
